7985f930e43561564109cc8618e20d0b3c8ddc2f
Matheus Oliveira <matheus.oliveira@tecnoval.com.br>
2023-04-05 20:34:05 -0300

    Adicionando background

--- src/pages/mainPage.jsx
+++ src/pages/mainPage.jsx
@@ -1,6 +1,13 @@
+import { useTheme } from "../hooks/useTheme";
+import BackgroundWhiteElange from "../assets/img/svg/elange-bg-white.svg";
+import BackgroundBlackElange from "../assets/img/svg/elange-bg-black.svg";
+
 const MainPage = () => {
+    const { theme } = useTheme();
     return (
-        <div className="w-full h-screen bg-black">....</div>
+        <div className={`w-full h-screen ${theme === "light" ? "bg-white" : "bg-black"} flex justify-center items-center`}>
+            <img src={theme === "light" ? BackgroundWhiteElange : BackgroundBlackElange} className="w-[60%] h-[50%]" alt="Background Elange" />
+        </div>
     );
 }
 export default MainPage;
