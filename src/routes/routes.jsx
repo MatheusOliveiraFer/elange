7240dd06d9504ac69f063f0dc43aca12fda5b11e
Matheus Oliveira <matheus.oliveira@tecnoval.com.br>
2023-04-10 09:48:31 -0300

    Atualização de logo

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -23,7 +23,7 @@ const main = () => {
                         <span className="sr-only">Open main menu</span>
                         <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
                     </button>
-                    <img src={theme === "light" ? ElangeSymbolBlack : ElangeSymbolWhite} className="w-[8%] h-[8%] mr-28" alt="Icon Instagram" />
+                    <img src={theme === "light" ? ElangeSymbolBlack : ElangeSymbolWhite} className="w-[8%] h-[8%] mr-28 max-sm:hidden" alt="Icon Instagram" />
                     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
                         <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-black md:dark:bg-black">
                             {theme === "light" ? (
